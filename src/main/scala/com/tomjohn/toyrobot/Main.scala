3b0e136ee596a382ed831c89bbc416537e057df6
Tom John <tom.john@rea-group.com>
2017-08-07 14:55:42 +1000

    I think I'm getting the pattern now - fixed and working

--- src/main/scala/com/tomjohn/toyrobot/Main.scala
+++ src/main/scala/com/tomjohn/toyrobot/Main.scala
@@ -1,6 +1,7 @@
 package com.tomjohn.toyrobot
 
 import scala.io.Source
+import scalaz.State
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -8,9 +9,14 @@ object Main {
     val t: Table = Table(Position(0, 0), Position(5, 5))
 
     val filename = "robot-test.in"
-    val lines = Source.fromFile(filename).getLines
+    val lines: Iterator[String] = Source.fromFile(filename).getLines
 
-    val r = Robot(Position(1, 3), North)
-    lines.foreach(l => t.doCommand(r, l))
+    val states: Iterator[State[Option[Robot], Unit]] = lines.map(l => t.doCommand(l))
+
+    val finalState: State[Option[Robot], Unit] = states.fold(State.state())(
+      (x, y) => x.flatMap(_ => y)
+    )
+
+    finalState.apply(Option.empty)
   }
 }
--- src/main/scala/com/tomjohn/toyrobot/Table.scala
+++ src/main/scala/com/tomjohn/toyrobot/Table.scala
@@ -3,7 +3,8 @@ package com.tomjohn.toyrobot
 import scalaz.State
 
 case class Table(bottomLeft: Position, topRight: Position) {
-  def doCommand(r: Robot, cmd: String): State[Option[Robot], Unit] = {
+
+  def doCommand(cmd: String): State[Option[Robot], Unit] = {
     val splitCmd = cmd.split(" ")
     splitCmd(0) match {
       case "PLACE" =>
@@ -11,19 +12,42 @@ case class Table(bottomLeft: Position, topRight: Position) {
         val newX = Integer.parseInt(placeCmd(0))
         val newY = Integer.parseInt(placeCmd(1))
         val newDir = Direction.lookup(placeCmd(2))
-        State.put(Some(Robot(Position(newX, newY), newDir)))
+        val newRobot = Robot(Position(newX, newY), newDir)
+        if (onTable(newRobot)) State.put(Some(newRobot)) else State.state(())
       case "MOVE" =>
-        State.put(Some(r.move))
+        adjustRobot(_.move, onTable)
       case "LEFT" =>
-        State.put(Some(r.left))
+        adjustRobot(_.left)
       case "RIGHT" =>
-        State.put(Some(r.right))
+        adjustRobot(_.right)
       case "REPORT" =>
-        r.report()
-        State.state(())
+        adjustRobot(_.report())
       case _ =>
         println("skip unknown command")
         State.state(())
     }
+  }
+
+  private def adjustRobot(action: Robot => Robot, accept: Option[Robot] => Boolean = always): State[Option[Robot], Unit] = {
+    for {
+      maybeRobot <- State.gets((o: Option[Robot]) => o.map(action))
+      _ <- if (accept(maybeRobot)) State.put(maybeRobot) else State.state[Option[Robot], Unit](())
+    } yield ()
+  }
+
+  private def always(o: Option[Robot]): Boolean = {
+    true
+  }
+
+  private def onTable(o: Option[Robot]): Boolean = {
+    o match {
+      case Some(r) => onTable(r)
+      case None => false
+    }
+  }
+
+  private def onTable(r: Robot): Boolean = {
+    r.p.x >= bottomLeft.x && r.p.x <= topRight.x &&
+      r.p.y >= bottomLeft.y && r.p.y <= topRight.y
   }
 }
