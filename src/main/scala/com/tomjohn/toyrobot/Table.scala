008194087e6f5f62ec2e25800aeae6f11a017984
Tom John <tom.john@rea-group.com>
2017-08-07 11:22:19 +1000

    WIP: Change doCommand to return State[Option[Robot], Unit] - need to calculate and apply final state

--- src/main/scala/com/tomjohn/toyrobot/Table.scala
+++ src/main/scala/com/tomjohn/toyrobot/Table.scala
@@ -1,7 +1,9 @@
 package com.tomjohn.toyrobot
 
+import scalaz.State
+
 case class Table(bottomLeft: Position, topRight: Position) {
-  def doCommand(r: Robot, cmd: String): Robot = {
+  def doCommand(r: Robot, cmd: String): State[Option[Robot], Unit] = {
     val splitCmd = cmd.split(" ")
     splitCmd(0) match {
       case "PLACE" =>
@@ -9,16 +11,19 @@ case class Table(bottomLeft: Position, topRight: Position) {
         val newX = Integer.parseInt(placeCmd(0))
         val newY = Integer.parseInt(placeCmd(1))
         val newDir = Direction.lookup(placeCmd(2))
-        Robot(Position(newX, newY), newDir)
+        State.put(Some(Robot(Position(newX, newY), newDir)))
       case "MOVE" =>
-        r.move
+        State.put(Some(r.move))
       case "LEFT" =>
-        r.left
+        State.put(Some(r.left))
       case "RIGHT" =>
-        r.right
+        State.put(Some(r.right))
+      case "REPORT" =>
+        r.report()
+        State.state(())
       case _ =>
         println("skip unknown command")
-        r
+        State.state(())
     }
   }
 }
