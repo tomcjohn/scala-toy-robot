ba752a826e911eafad8109dc5a10630edeef3643
Tom John <tom.john@rea-group.com>
2017-08-07 10:25:20 +1000

    Added Position, Direction and Robot - still need to add Table

---
+++ src/main/scala/com/tomjohn/toyrobot/Direction.scala
@@ -0,0 +1,31 @@
+package com.tomjohn.toyrobot
+
+sealed trait Direction {
+  def move(p: Position): Position
+  def left: Direction
+  def right: Direction
+}
+
+case object North extends Direction {
+  def move(p: Position): Position = Position(p.x, p.y + 1)
+  def left: Direction = West
+  def right: Direction = East
+}
+
+case object South extends Direction {
+  def move(p: Position): Position = Position(p.x, p.y - 1)
+  def left: Direction = East
+  def right: Direction = West
+}
+
+case object East extends Direction {
+  def move(p: Position): Position = Position(p.x + 1, p.y)
+  def left: Direction = North
+  def right: Direction = South
+}
+
+case object West extends Direction {
+  def move(p: Position): Position = Position(p.x - 1, p.y)
+  def left: Direction = South
+  def right: Direction = North
+}
--- src/main/scala/com/tomjohn/toyrobot/Main.scala
+++ src/main/scala/com/tomjohn/toyrobot/Main.scala
@@ -1,7 +1,13 @@
 package com.tomjohn.toyrobot
 
+import scala.io.Source
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+
+    val filename = "robot-test.in"
+    val lines = Source.fromFile(filename).getLines
+
+    lines.foreach(l => println(l))
   }
 }
